f151172fb1c8b0cbcb7dd407c444b071bed81b60
sebastianojeda <sebastianojeda.school@gmail.com>
2015-01-22 11:44:40 -0800

    putting in an entity

--- js/screens/play.js
+++ js/screens/play.js
@@ -5,8 +5,10 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
-
 		me.levelDirector.loadLevel("test");
+
+		var player = me.pool.pull("player", 0, 420, {});
+		me.game.world.addChild(player, 5);
 
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
