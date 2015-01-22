8be88cba10c767b252d5e30905d4b6bfea29a161
sebastianojeda <sebastianojeda.school@gmail.com>
2015-01-22 12:28:21 -0800

    got my entity to show and move

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,6 +1,6 @@
 game.PlayerEntity = me.Entity.extend({
 	init: function(x, y, settings){
-		this._super(me.Entity, "init", [x, y{
+		this._super(me.Entity, 'init', [x, y,{
 			image: "player",
 			width: 64,
 			height: 64,
@@ -10,9 +10,23 @@ game.PlayerEntity = me.Entity.extend({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
+	  		this.body.setVelocity(5, 0);
+
+
 	},
 
-	update: function(){
+	update: function(delta){
+		if(me.input.isKeyPressed("right")){
+			//sets the position of my X by adding the volocity above in 
+			//setVelocity() and multiplying it by me.timer.tick.
+			//me.timer.tick makes the movement look smooth
+			this.body.vel.x += this.body.accel.x * me.timer.tick;
+		}
+		else{
+			this.body.vel.x = 0;
+		}
 
+		this.body.update(delta);
+		return true;
 	}
 });
