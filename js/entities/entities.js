44643e151bf3958ea1376ce70741b329c095ddbb
sebastianojeda <sebastianojeda.school@gmail.com>
2015-02-10 20:31:09 -0800

    Finished video 19 everything works

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -221,16 +221,37 @@ game.EnemyCreep = me.Entity.extend({
 		this.health = 10;
 		this.alwaysUpdate = true;
 
-		this.setVelocity(3, 20);
+		this.body.setVelocity(3, 20);
 
 		this.type = "EnemyCreep";
 
 		this.renderable.addAnimation("walk", [3, 4, 5], 80);
 		this.renderable.setCurrentAnimation("walk");
-		
+
 	},
 
 	update: function(){
 
+	}
+});
+
+game.GameManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date().getTime();
+		this.lastCreep = new Date().getTime();
+
+		this.alwaysUpdate = true;
+
+	},
+
+	update: function(){
+		this.now = new Date().getTime();
+		//Math.round works as a timer to properly send 
+		//enemy creeps at a given time
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creepe, 5);
+		}
+		return true;
 	}
 });
