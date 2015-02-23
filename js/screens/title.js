d219443879b7aa75a6914393a24bf3482a19d650
sebastianojeda <sebastianojeda.school@gmail.com>
2015-02-23 14:58:55 -0800

    finished vid 27

--- js/screens/title.js
+++ js/screens/title.js
@@ -3,7 +3,27 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		 me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen'))); // TODO
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+	
+		me.input.bindKey(me.input.KEY.ENTER, 'start')
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+				this.font = new me.Font('Arial', 46, 'white');
+			},
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), 'Awesomenauts', 450, 130);
+				this.font.draw(renderer.getContext(), 'Press ENTER to play', 250,530);
+			}
+		})));
+
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keycode, edge){
+			if(action === 'state'){
+				me.state.change(me.state.PLAY);
+			}
+		});
+
 	},
 	
 	
@@ -11,6 +31,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKEY(me.input.KEY.ENTER); // TODO
+		me.event.unsubcribe(this.handler);
 	}
 });
