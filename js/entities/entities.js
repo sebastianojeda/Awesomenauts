837bc479763bcc887103df2cb65721bad76cb6dc
sebastianojeda <sebastianojeda.school@gmail.com>
2015-02-27 12:28:14 -0800

    finished vid 33

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -134,62 +134,91 @@ game.PlayerEntity = me.Entity.extend({
 			this.health = this.health - damage;		
 		},
 
+
+
+
+
+
+
+
+
 		//collideHandler is responsable for make my player 
 		//collide with the bases
 	collideHandler: function(response){
 		if(response.b.type==="EnemyBase"){
-			var ydif = this.pos.y - response.b.pos.y;
-			var xdif = this.pos.x - response.b.pos.x;
+
+			this.collideWithEnemyBase(response);
+
+		}else if(response.b.type==='EnemyCreep'){
+			this.collideWithEnemyCreep(response);
+		}
+	},
+
+	collideWithEnemyBase: function(response){
+		var ydif = this.pos.y - response.b.pos.y;
+		var xdif = this.pos.x - response.b.pos.x;
 
 				
-		    if(ydif<-40 && xdif< 70 && xdif>-35){
+	    if(ydif<-40 && xdif< 70 && xdif>-35){
 				this.body.falling = false;
 				// this.body.vel.y = -1;
 			}
-			else if(xdif>-35 && this.facing=== "right" && (xdif<0) && ydif>-50){
+		else if(xdif>-35 && this.facing=== "right" && (xdif<0) && ydif>-50){
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x -1;
-			}else if(xdif<70 && this.facing=== "left" && (xdif>0)){
+		}else if(xdif<70 && this.facing=== "left" && (xdif>0)){
 				this.body.vel.x = 0;
 				// this.pos.x = this.pos.x +1;	
 			}
-			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
-				     &&(Math.abs(ydif) <=40) && 
-				    ( ( (xdif>0) && this.facing==='left') || ( (xdif<0) && this.facing==='right'))
-				     ){
+		if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
+				&&(Math.abs(ydif) <=40) && 
+				( ( (xdif>0) && this.facing==='left') || ( (xdif<0) && this.facing==='right'))
+			    ){
 				
 				this.lastHit = this.now;
 				response.b.loseHealth(game.data.playerAttack);
 			}
-		}else if(response.b.type==='EnemyCreep'){
-			var xdif = this.pos.x - response.b.pos.x;
-			var ydif = this.pos.y - response.b.pos.y;
+	},
 
-			if (xdif>0){
-				// this.pos.x = this.pos.x + 1;
-				if(this.facing==='left'){
-					this.body.vel.x = 0;
-				}
-			}else{
-				if(this.faceing==='right'){
-					this.body.vel.x = 0;
-				}
-				// this.pos.x = this.pos.x -1;
+	collideWithEnemyCreep: function(response){
+		var xdif = this.pos.x - response.b.pos.x;
+		var ydif = this.pos.y - response.b.pos.y;
+
+		this.stopMovement(xdif);
+
+		if(this.checkAttack(xdif, ydif)){
+			this.hitCreep(response);
+		};
+	},
+
+	stopMovement: function(xdif){
+		if (xdif>0){
+			if(this.facing==='left'){
+				this.body.vel.x = 0;
 			}
-
-			//This if statement is created to make the
-			//enemy creep lose health
-			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >=game.data.playerAttackTimer){
-				this.lastHit = this.now;
-					//if creeps health is less than our attack, execute if statement
-				if(response.b.health <= game.data.playerAttack){
-					//adds one gold for player
-					game.data.gold += 1;
-					
-				}
-
-				response.b.losehealth(game.data.playerAttack);
+		}else{
+			if(this.faceing==='right'){
+				this.body.vel.x = 0;
 			}
 		}
+	},
+
+	checkAttack: function(xdif, ydif){
+		//This if statement is created to make the
+		//enemy creep lose health
+		if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >=game.data.playerAttackTimer){
+			this.lastHit = this.now;
+				return true;
+			}
+			return false;
+	},
+	hitCreep: function(response){
+		//if creeps health is less than our attack, execute if statement
+		if(response.b.health <= game.data.playerAttack){
+			//adds one gold for player
+			game.data.gold += 1;					
+		}
+	    response.b.losehealth(game.data.playerAttack);
+
 	}		
 });
