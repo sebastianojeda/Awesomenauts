6df1e56eb4e5087b701bbe3af39a9b444d29e96d
sebastianojeda <sebastianojeda.school@gmail.com>
2015-03-10 12:28:01 -0700

    almost done with vid 43

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -14,7 +14,12 @@ game.spendExp = me.ScreenObject.extend({
 
 			draw: function(renderer){
 				this.font.draw(renderer.getContext(), 'Awesomenauts', 400, 130);
-				this.font.draw(renderer.getContext(), 'SPEND', this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), 'Press F1-F4 TO BUY, F5 TO SKIP', this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), 'CURRENT EXP:' + game.data.exp.toString(), this.pos.x =100, this.pos.y + 50);
+				this.font.draw(renderer.getContext(), 'CURRENT EXP:' + game.data.exp.toString(), this.pos.x =100, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), 'CURRENT EXP:' + game.data.exp.toString(), this.pos.x =100, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), 'CURRENT EXP:' + game.data.exp.toString(), this.pos.x =100, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), 'CURRENT EXP:' + game.data.exp.toString(), this.pos.x =100, this.pos.y + 50);
 			},
 
 		
