c8c3b13dda0f36758f4b637fbd46880512496c1d
sebastianojeda <sebastianojeda.school@gmail.com>
2015-03-11 12:27:30 -0700

    almost done with vid 44

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -11,7 +11,7 @@ game.spendExp = me.ScreenObject.extend({
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
 				this._super(me.Renderable, 'init', [270, 240, 300, 50]);
-				this.font = new me.Font('Arial', 26, 'white');
+				this.font = new me.Font('Arial', 26, 'black');
 				
 			},
 
@@ -19,7 +19,7 @@ game.spendExp = me.ScreenObject.extend({
 				this.font.draw(renderer.getContext(), 'Awesomenauts', 400, 130);
 				this.font.draw(renderer.getContext(), 'Press F1-F4 TO BUY, F5 TO SKIP', this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), 'CURRENT EXP:' + game.data.exp.toString(), this.pos.x, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), 'F1: INCREASE GOLD' , this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), 'F1: INCREASE GOLD CURRENT LEVEL' + game.data.exp1.toString() + "COST: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
 				this.font.draw(renderer.getContext(), 'F2: POWER UPS' , this.pos.x, this.pos.y + 150);
 				this.font.draw(renderer.getContext(), 'F3: INCREASE DAMAGE' , this.pos.x, this.pos.y + 200);
 				this.font.draw(renderer.getContext(), 'F4: INCREASE HEALTH' , this.pos.x, this.pos.y + 250);
