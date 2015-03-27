4f8a540001223aa3248ca92d6a08802957608ee6
sebastianojeda <sebastianojeda.school@gmail.com>
2015-03-27 11:02:08 -0700

    done with vid 50

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -100,6 +100,8 @@ game.HeroDeathManager = Object.extend({
 			}
  		}
 
+ 		 this.checkBuyKeys();
+
  		return true;
  	},
 
@@ -155,6 +157,89 @@ game.HeroDeathManager = Object.extend({
  		me.input.unbindKey(me.input.KEY.F1, 'F5', true);
  		me.input.unbindKey(me.input.KEY.F1, 'F6', true);
  		me.game.world.removeChild(game.data.buytext);
+
+ 	},
+
+ 	checkBuyKeys: function(){
+ 		if(me.input.isKeyPressed("F1")){
+ 			if(this.checkCost(1)){
+ 				this.makePurchase(1);
+ 			}
+ 		}else if(me.input.isKeyPressed("F2")){
+ 			if(this.checkCost(2)){
+ 				this.makePurchase(2);
+ 			}
+ 		}else if(me.input.isKeyPressed("F3")){
+ 			if(this.checkCost(3)){
+ 				this.makePurchase(3);
+ 			}
+ 		}else if(me.input.isKeyPressed("F4")){
+ 			if(this.checkCost(4)){
+ 				this.makePurchase(4);
+ 			}
+ 		}else if(me.input.isKeyPressed("F5")){
+ 			if(this.checkCost(5)){
+ 				this.makePurchase(5);
+ 			}
+ 		}else if(me.input.isKeyPressed("F6")){
+ 			if(this.checkCost(6)){
+ 				this.makePurchase(6);
+ 			}
+ 		}
+ 	},
+
+ 	checkCost: function(skill){
+ 		if(skill===1 && ( game.data.gold>= ((game.data.skill1+1)* 10) )){
+
+ 			return true;
+
+ 		}else if(skill===2 && ( game.data.gold>= ((game.data.skill2+1)* 10) )){
+
+ 			return true;
+
+ 		}else if(skill===3 && ( game.data.gold>= ((game.data.skill3+1)* 10) )){
+
+ 			return true;
+
+ 		}else if(skill===4 && ( game.data.gold>= ((game.data.ability1+1)* 10) )){
+
+ 			return true;
+
+ 		}else if(skill===5 && ( game.data.gold>= ((game.data.ability2+1)* 10) )){
+
+ 			return true;
+
+ 		}else if(skill===6 && ( game.data.gold>= ((game.data.ability3 +1)* 10) )){
+
+ 			return true;
+
+ 		}
+ 		else{
+ 			return false;
+ 		}
+ 	},
+
+ 	makePurchase: function(){
+ 		if(skill ===  1){
+ 		game.data.gold -= ((game.data.skill1 +1)* 10)
+ 		game.data.skill += 1;
+ 		game.data.player.attack += 1;
+ 	}else if(skill === 2){
+ 		game.data.gold -= ((game.data.skill2 +1)* 10)
+ 		game.data.skill += 1;
+ 	}else if(skill === 3){
+ 		game.data.gold -= ((game.data.skill3 +1)* 10)
+ 		game.data.skill += 1;
+ 	}else if(skill === 4){
+ 		game.data.gold -= ((game.data.ability1 +1)* 10)
+ 		game.data.skill += 1;
+ 	}else if(skill === 5){
+ 		game.data.gold -= ((game.data.ability2 +1)* 10)
+ 		game.data.skill += 1;
+ 	}else if(skill === 6){
+ 		game.data.gold -= ((game.data.ability3 +1)* 10)
+ 		game.data.skill += 1;
+ 	}
 
  	}
  });
